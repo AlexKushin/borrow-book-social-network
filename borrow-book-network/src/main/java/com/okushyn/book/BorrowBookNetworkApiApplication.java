f12f20a943ac13c21b4178b45680e9d6e153418c
kyshin <kyshin1993@gmail.com>
2024-10-30 01:14:46 +0100

    Initial commit

---
+++ borrow-book-network/src/main/java/com/okushyn/book/BorrowBookNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.okushyn.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrowBookNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorrowBookNetworkApiApplication.class, args);
+	}
+
+}
