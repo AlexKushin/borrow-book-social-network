e53bc2083e016f05582ffb1433817472cd462583
kyshin <kyshin1993@gmail.com>
2024-11-06 22:58:19 +0100

    User, Role and Token classes have been created and relations between them has been set up. Jpa Repositories for User, Role and Token have been creatd

--- borrow-book-network/src/main/java/com/okushyn/book/BorrowBookNetworkApiApplication.java
+++ borrow-book-network/src/main/java/com/okushyn/book/BorrowBookNetworkApiApplication.java
@@ -2,8 +2,10 @@ package com.okushyn.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BorrowBookNetworkApiApplication {
 
 	public static void main(String[] args) {
