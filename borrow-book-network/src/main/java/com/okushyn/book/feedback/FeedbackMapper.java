c464d803a227b950f4548e4e61ad67be5bf1ec7e
kyshin <kyshin1993@gmail.com>
2024-11-27 20:36:13 +0100

    FeedbackController, FeedbackMapper, FeedbackRepository, FeedbackRequest, FeedbackService have been implemented. Logic for saving new feedback has been implemented

---
+++ borrow-book-network/src/main/java/com/okushyn/book/feedback/FeedbackMapper.java
@@ -0,0 +1,17 @@
+package com.okushyn.book.feedback;
+
+import com.okushyn.book.book.Book;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FeedbackMapper {
+    public Feedback toFeedback(FeedbackRequest request) {
+        return Feedback.builder()
+                .note(request.note())
+                .comment(request.comment())
+                .book(Book.builder()
+                        .id(request.bookId())
+                        .build())
+                .build();
+    }
+}
