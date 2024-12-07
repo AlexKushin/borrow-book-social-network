2b1b7b7f921cdafeb0a25e703138102a3a44737c
kyshin <kyshin1993@gmail.com>
2024-12-07 22:24:54 +0100

    login component has been implemented

---
+++ borrow-book-social-network-ui/src/app/pages/login/login.component.ts
@@ -0,0 +1,49 @@
+import {Component} from '@angular/core';
+import {AuthenticationRequest} from "../../services/models/authentication-request";
+import {NgForOf, NgIf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/services/authentication.service";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    NgIf,
+    NgForOf,
+    FormsModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  authRequest: AuthenticationRequest = {email: '', password: ''};
+  errorMessage: Array<string> = [];
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {
+  }
+
+  login() {
+    this.errorMessage = [];
+    this.authService.authenticate(
+      {
+        body: this.authRequest
+      }
+    ).subscribe({
+      next: () => {
+        this.router.navigate(['books'])
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+  register() {
+    this.router.navigate(['register'])
+  }
+}
