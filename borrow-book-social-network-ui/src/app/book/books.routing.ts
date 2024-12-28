4e0a3da882440a5c3b7188b176a5871cfc22e32b
kyshin <kyshin1993@gmail.com>
2024-12-29 00:26:22 +0100

    My books component has been implemented

--- borrow-book-social-network-ui/src/app/book/books.routing.ts
+++ borrow-book-social-network-ui/src/app/book/books.routing.ts
@@ -1,15 +1,20 @@
 import {Routes} from "@angular/router";
 import {MainComponent} from "./pages/main/main.component";
 import {BookListComponent} from "./pages/book-list/book-list.component";
+import {MyBooksComponent} from "./pages/my-books/my-books.component";
 
 export const BOOK_ROUTES: Routes = [{
   path: '',
   component: MainComponent,
   children: [
-     {
-       path: '',
-       component: BookListComponent
-     }
+    {
+      path: '',
+      component: BookListComponent
+    },
+    {
+      path: 'my-books',
+      component: MyBooksComponent
+    }
   ]
 }];
 
