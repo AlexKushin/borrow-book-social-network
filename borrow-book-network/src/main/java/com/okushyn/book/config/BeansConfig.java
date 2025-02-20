50f2ac38c5fb0cac0c446073c1fc9822ebd454f1
kyshin <kyshin1993@gmail.com>
2025-02-20 23:47:32 +0100

    BeanConfig has bin refactored Version has been updated

--- borrow-book-network/src/main/java/com/okushyn/book/config/BeansConfig.java
+++ borrow-book-network/src/main/java/com/okushyn/book/config/BeansConfig.java
@@ -1,6 +1,7 @@
 package com.okushyn.book.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -25,6 +26,9 @@ import static org.springframework.http.HttpHeaders.*;
 public class BeansConfig {
 
     private final UserDetailsService userDetailsService;
+
+    @Value("${application.cors.origins}")
+    private List<String> allowedOrigins;
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
@@ -53,8 +57,8 @@ public class BeansConfig {
     public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:8080"));
+        //config.setAllowCredentials(true); todo <- should be learned before deleted
+        config.setAllowedOrigins(allowedOrigins);
         config.setAllowedHeaders(Arrays.asList(
                 ORIGIN,
                 CONTENT_TYPE,
